4729893228eca7898d47122f298eeeb80bd57c44
Jovillios <decaestecker.jules@gmail.com>
2025-01-08 18:39:53 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
-mv kaggle.json /root/.kaggle/
-
-# Set permissions
-chmod 600 /root/.kaggle/kaggle.json
-
 kaggle competitions download -c generating-graphs-with-specified-properties
 unzip -q generating-graphs-with-specified-properties.zip
